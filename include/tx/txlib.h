f5809117cb51108d9abc2acae1eac47f55be53e1
TX_Jerry <junyuanliu655@gmail.com>
2026-05-08 23:04:01 -0400

    add the umbrella header: tx/txlib.h

---
+++ include/tx/txlib.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2025 TXLib. Licensed under the MIT License.
+
+#pragma once
+#include "tx/math.h"
+#include "tx/map.h"
+#include "tx/json.h"
+#include "tx/grid.h"
+#include "tx/resource.h"
+#include "tx/data.h"
+#include "tx/graphics.h"
+#include "tx/utility.h"
