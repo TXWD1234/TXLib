b5aa10cf9cb3d20642db6e903d107b751b152572
TX_Jerry <junyuanliu655@gmail.com>
2026-05-17 22:43:51 -0400

    InstallationFinal #2: update log.cmake

--- TXCMake/installation/log.cmake
+++ TXCMake/installation/log.cmake
@@ -2,12 +2,15 @@ include_guard(GLOBAL)
 
 function(tx_log)
 	string(JOIN "\nTXLib: " messageStr ${ARGN})
-
 	message(STATUS "TXLib: ${messageStr}")
 endfunction()
 
 function(tx_error_log moduleName)
-	string(JOIN "\nTXLib:   " messageStr ${ARGN})
-
-	message(FATAL_ERROR "TXLib: In module: [${moduleName}]:\nTXLib:   ${messageStr}")
+	if(moduleName)
+		string(JOIN "\nTXLib:   " messageStr ${ARGN})
+		message(FATAL_ERROR "TXLib: In module: [${moduleName}]:\nTXLib:   ${messageStr}")
+	else()
+		string(JOIN "\nTXLib: " messageStr ${ARGN})
+		message(FATAL_ERROR "TXLib: ${messageStr}")
+	endif()
 endfunction()
