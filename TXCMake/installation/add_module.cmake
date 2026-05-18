e2974fec4dfc202715ce215ee6ff29825900a69b
TX_Jerry <junyuanliu655@gmail.com>
2026-05-17 22:44:32 -0400

    InstallationFinal #3: update add_module.cmake

---
+++ TXCMake/installation/add_module.cmake
@@ -0,0 +1,9 @@
+include_guard(GLOBAL)
+
+# this function is simply just the wrapper of add_subdirectory
+function(tx_add_module MODULE)
+	set(TXLib_INSTALLATION_MODULE_NAME ${MODULE})
+	add_subdirectory(
+		"${TXLib_SOURCE_DIR}/modules/${TXLib_${MODULE}_SOURCE_DIR}"
+		"${TXLib_BINARY_DIR}/${MODULE}")
+endfunction()
