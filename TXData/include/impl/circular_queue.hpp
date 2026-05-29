d6eb1315011d5eda9f49dcbcb10fd32f6945c7c2
TX_Jerry <junyuanliu655@gmail.com>
2026-05-29 12:47:03 -0400

    TXData: CircularQueue: refactor to adopt the new overlay pattern

--- TXData/include/impl/circular_queue.hpp
+++ TXData/include/impl/circular_queue.hpp
@@ -8,7 +8,6 @@
 
 namespace tx {
 /**
- * This class doesn't own the data, but it manipulate it
  * Everytime before you push, you should check `full()`.
  *   Any push operation on an full CircularQueue is Undefined Behavior
  * Everytime before you pop, you should check `empty()`.
@@ -17,25 +16,41 @@ namespace tx {
 template <class T>
 class CircularQueueOverlay {
 public:
-	CircularQueueOverlay(std::span<T> data)
-	    : m_data(data) {}
-
 	using value_type = T;
+
+public:
+	/**
+	 * @param ptr the data pointer to a piece of memory that have at least size of `capacity`.
+	 * @param capacity the capacity of this container object. It cannot resize.
+	 */
+	CircularQueueOverlay(T* ptr, u32 size)
+	    : m_data(ptr), m_size(size) {}
+	/**
+	 * @param buffer the provided storage memory buffer
+	 */
+	CircularQueueOverlay(std::span<T> buffer)
+	    : m_data(buffer.data()), m_size(buffer.size()) {}
+	~CircularQueueOverlay() {}
+
+	CircularQueueOverlay(const CircularQueueOverlay&) = default;
+	CircularQueueOverlay& operator=(const CircularQueueOverlay&) = default;
+	CircularQueueOverlay(CircularQueueOverlay&& other) = default;
+	CircularQueueOverlay& operator=(CircularQueueOverlay&& other) = default;
 
 	// basic operations
 
 	void push(const T& val) {
-		m_data[m_end] = val;
+		at_impl(m_end) = val;
 		push_impl();
 	}
 	void push(T&& val) {
-		m_data[m_end] = std::move(val);
+		at_impl(m_end) = std::move(val);
 		push_impl();
 	}
 
 	void pop() {
 		if constexpr (!std::is_trivial_v<T>) {
-			m_data[m_begin] = T{};
+			at_impl(m_begin) = T{};
 		}
 		pop_impl();
 	}
@@ -50,33 +65,83 @@ public:
 		m_wrap = false;
 	}
 
-
 	// basic getter
 
 	bool full() const { return m_begin == m_end && m_wrap; }
 	bool empty() const { return m_begin == m_end && !m_wrap; }
 	u32 size() const {
 		return (m_wrap ?
-		            m_data.size() - m_begin + m_end :
+		            m_size - m_begin + m_end :
 		            m_end - m_begin);
 	}
 
-
-private:
-	std::span<T> m_data;
+protected:
+	T* m_data;
+	u32 m_size;
 	u32 m_begin = 0, m_end = 0;
 	bool m_wrap = false;
 
+	void null_impl() {
+		m_data = nullptr;
+		m_size = 0;
+		m_begin = 0;
+		m_end = 0;
+		m_wrap = false;
+	}
+	void swap_impl(CircularQueueOverlay<T>& other) {
+		std::swap(m_data, other.m_data);
+		std::swap(m_size, other.m_size);
+		std::swap(m_begin, other.m_begin);
+		std::swap(m_end, other.m_end);
+		std::swap(m_wrap, other.m_wrap);
+	}
+	// copy the data and state of another object after construction
+	// to fully sync with the other object
+	void copy_impl(const CircularQueueOverlay<T>& other) {
+		if (other.m_wrap) {
+			std::uninitialized_copy(
+			    other.m_data + other.m_begin,
+			    other.m_data + other.m_size,
+			    m_data + other.m_begin);
+			std::uninitialized_copy(
+			    other.m_data,
+			    other.m_data + other.m_end,
+			    m_data);
+		} else {
+			std::uninitialized_copy(
+			    other.m_data + other.m_begin,
+			    other.m_data + other.m_end,
+			    m_data);
+		}
+		m_begin = other.m_begin;
+		m_end = other.m_end;
+		m_wrap = other.m_wrap;
+	}
+	// query if object is valid
+	// used for defend moved-from object
+	bool isNull_impl() const {
+		return !m_data;
+	}
+	// called at destruction to clean up data
+	void destruct_impl() {
+		clear();
+	}
+
+private:
+	T& at_impl(u32 index) {
+		return *(m_data + index);
+	}
+
 	void push_impl() {
 		m_end++;
-		if (m_end == m_data.size()) {
+		if (m_end == m_size) {
 			m_end = 0; // wrapping logic
 			m_wrap = true;
 		}
 	}
 	void pop_impl() {
 		m_begin++;
-		if (m_begin == m_data.size()) {
+		if (m_begin == m_size) {
 			m_begin = 0; // wrapping logic
 			m_wrap = false;
 		}
@@ -85,42 +150,52 @@ private:
 	template <std::invocable<T&> Func>
 	void foreach_impl(Func&& f) {
 		if (m_wrap) {
-			for (u32 i = m_begin; i < m_data.size(); i++) {
-				f(m_data[i]);
+			for (u32 i = m_begin; i < m_size; i++) {
+				f(at_impl(i));
 			}
 			for (u32 i = 0; i < m_end; i++) {
-				f(m_data[i]);
+				f(at_impl(i));
 			}
 		} else {
 			for (u32 i = m_begin; i < m_end; i++) {
-				f(m_data[i]);
+				f(at_impl(i));
 			}
 		}
 	}
 };
 
 template <class T>
-class CircularQueue {
+class CircularQueue : public CircularQueueOverlay<T> {
 public:
 	CircularQueue(u32 capacity)
-	    : m_data(capacity), m_overlay(m_data) {}
-	using value_type = T;
+	    : CircularQueueOverlay<T>(
+	          allocate<T>(capacity), capacity) {}
+	~CircularQueue() {
+		if (!this->isNull_impl()) {
+			this->destruct_impl(); // destroy live elements before freeing
+			free(this->data());
+		}
+	}
 
-	void push(const T& val) { m_overlay.push(val); }
-	void push(T&& val) { m_overlay.push(std::move(val)); }
-
-	void pop() { m_overlay.pop(); }
-
-	void clear() { m_overlay.clear(); }
-
-	// basic getter
-
-	bool full() const { return m_overlay.full(); }
-	bool empty() const { return m_overlay.empty(); }
-	u32 size() const { return m_overlay.size(); }
+	CircularQueue(const CircularQueue<T>& other)
+	    : CircularQueueOverlay<T>(
+	          allocate<T>(other.m_size), other.m_size) {
+		copy_impl(other);
+	}
+	CircularQueue(CircularQueue<T>&& other) : CircularQueueOverlay<T>(other) {
+		// just use the copy constructor of CircularQueueOverlay - shallow copy
+		other.null_impl();
+	}
+	CircularQueue& operator=(CircularQueue<T> other) {
+		this->swap_impl(other);
+		return *this;
+	}
 
 private:
-	std::vector<T> m_data;
-	CircularQueueOverlay<T> m_overlay;
+	// cannot be swap_impl because ambiguity with base's swap_impl
+	// this function exists for potential future expansion
+	void swap(CircularQueue<T>& other) {
+		swap_impl(other);
+	}
 };
 } // namespace tx
