8e1529e91118d5ddafdee2aa6e6759008b7c3ffe
TX_Jerry <junyuanliu655@gmail.com>
2026-07-14 15:34:27 +0800

    Complete CMake Refactor Of Modules - TXGrid - TXResource - TXJson

--- module_registry.cmake
+++ module_registry.cmake
@@ -4,4 +4,44 @@ include("${TXLib_INSTALLATION_DIR}/module_registry_register.cmake")
 
 set(TXLib_MODULES)
 
-# <-------------- write the registry for all modules
+tx_module_registry_register(
+	TXFoundation "TXFoundation"
+)
+tx_module_registry_register(
+	TXMath "TXMath"
+	TXFoundation
+)
+tx_module_registry_register(
+	TXData "TXData"
+	TXFoundation
+)
+tx_module_registry_register(
+	TXUtility "TXUtility"
+	TXFoundation
+	TXData
+)
+tx_module_registry_register(
+	TXGraphics "TXGraphics"
+	TXFoundation
+	TXMath
+	TXData
+	TXUtility
+)
+tx_module_registry_register(
+	TXGrid "TXGrid"
+	TXMath
+	TXData
+	TXUtility
+)
+tx_module_registry_register(
+	TXResource "TXResource"
+	TXMath
+	TXData
+	TXUtility
+)
+tx_module_registry_register(
+	TXJson "TXJson"
+	TXMath
+	TXData
+	TXUtility
+)
