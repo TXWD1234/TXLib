603ac0325bdeaaa76b32b7ca927c396a881fb4ce
TX_Jerry <junyuanliu655@gmail.com>
2026-09-09 13:27:07 -0400

    TXData: Freelist: Refactor against the newest standard

--- modules/TXData/include/impl/freelist.hpp
+++ modules/TXData/include/impl/freelist.hpp
@@ -5,6 +5,7 @@
 #include "impl/data_utils.hpp"
 #include "tx/basic_types.hpp"
 #include "tx/exception.hpp"
+#include <memory>
 #include <concepts>
 #include <format>
 
@@ -29,22 +30,45 @@ public:
 
 public:
 	FreelistOverlay(T* bufferPtr, SizeT bufferSize, StateStorage* statePtr)
-	    : m_data(bufferPtr),
-	      m_state(std::construct_at(reinterpret_cast<State_impl*>(statePtr))),
-	      m_dataSize(bufferSize) {
-		impl::assert_impl([&] { return bufferPtr && bufferSize && statePtr; },
-		                  "Invalid Object. Invalid pointer or size provided.");
+	    : FreelistOverlay<T>(statePtr, bufferPtr, bufferSize) {
+		std::construct_at(m_state);
+	}
+	FreelistOverlay(std::span<T> buffer, StateStorage* statePtr)
+	    : FreelistOverlay<T>(buffer.data(), buffer.size(), statePtr) {}
+
+	FreelistOverlay() : m_data(nullptr), m_state(nullptr), m_dataSize(0) {}
+	~FreelistOverlay() {}
+
+	FreelistOverlay(const FreelistOverlay&) = default;
+	FreelistOverlay& operator=(const FreelistOverlay&) = default;
+	FreelistOverlay(FreelistOverlay&& other) = default;
+	FreelistOverlay& operator=(FreelistOverlay&& other) = default;
+
+	static FreelistOverlay<T> fromExistingState(
+	    T* bufferPtr, SizeT bufferSize, StateStorage* statePtr) {
+		FreelistOverlay<T> obj(statePtr, bufferPtr, bufferSize);
+		obj.m_state = std::launder(obj.m_state);
+		return obj;
+	}
+	static FreelistOverlay<T> fromExistingState(
+	    std::span<T> buffer, StateStorage* statePtr) {
+		FreelistOverlay<T> obj(statePtr, buffer.data(), buffer.size());
+		obj.m_state = std::launder(obj.m_state);
+		return obj;
 	}
 
+	bool valid() const { return m_data && m_dataSize && m_state; }
+
+	void destruct() {
+		impl::assert_impl(impl::assert::overlay_object_valid(this));
+		std::destroy_at(m_state);
+	}
 
 public:
 	SizeT allocate() {
+		impl::assert_impl(impl::assert::overlay_object_valid(this));
 		if (m_state->freeCount == 0) { // no free slot, bump
-			impl::assert_impl(
-			    [&] { return m_state->size < m_dataSize; },
-			    [&] { return std::format(
-				          "Size overflowed capacity. size = {}; capacity = {}",
-				          m_state->size, m_dataSize); });
+			impl::assert_impl(impl::assert::bad_expansion(m_state->size, m_dataSize));
 			return m_state->size++;
 		} else {
 			m_state->freeCount--;
@@ -58,6 +82,7 @@ public:
 	// accessing freed slot is UB
 	// object originally at the freeing slot must be already destroyed
 	void free(SizeT index) {
+		impl::assert_impl(impl::assert::overlay_object_valid(this));
 		impl::assert_impl(impl::assert::out_of_range(m_state->size, index));
 
 		// index at back, pop
@@ -71,7 +96,10 @@ public:
 		m_state->freeHead = index;
 	}
 
-	SizeT size() const { return m_state->size - m_state->freeCount; }
+	SizeT size() const {
+		impl::assert_impl(impl::assert::overlay_object_valid(this));
+		return m_state->size - m_state->freeCount;
+	}
 	SizeT capacity() const { return m_dataSize; }
 	bool empty() const { return !size(); }
 	bool full() const { return size() == capacity(); }
@@ -81,7 +109,26 @@ private:
 	State_impl* m_state;
 	SizeT m_dataSize;
 
+	FreelistOverlay(StateStorage* statePtr, T* bufferPtr, SizeT bufferSize)
+	    : m_data(bufferPtr),
+	      m_state(reinterpret_cast<State_impl*>(statePtr)),
+	      m_dataSize(bufferSize) {
+		impl::assert_impl(
+		    [this] { return this->valid(); },
+		    [=] {
+			    return std::format(
+			        "Bad construction. Invalid pointer or size provided."
+			        " bufferPtr = {}; bufferSize = {}; statePtr = {}",
+			        static_cast<const void*>(bufferPtr), bufferSize,
+			        static_cast<const void*>(statePtr));
+		    });
+	}
+
 	SizeT* metaAt_impl(SizeT index) {
+		// std::launder is intentinally missing because constuction also need
+		// to use this function. std::launder is expected to be used around
+		// this function when it's used for any operation other than
+		// construction
 		return reinterpret_cast<SizeT*>(m_data + index);
 	}
 };
